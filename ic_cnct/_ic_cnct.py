c6c4386f20521d58c549c002b6800e098b207b2b
yuanchaov <yuanchaov@gmail.com>
2024-03-01 17:05:14 +0100

    Add multi blks gen, Add json file, fix the str
    Add suport for struct
    
    json reformat
    
    json update

--- ic_cnct/_ic_cnct.py
+++ ic_cnct/_ic_cnct.py
@@ -1,12 +1,30 @@
 import re
 import sys
+import json
 
 class ic_cnct:
     def __init__(self, **kwargs):
         self.module_info = 0
         self.indent_default = kwargs.setdefault("indent_default", 4)
         self.indent = "    "    # later change
+        self.debug = False
 
+        self.top_json = []
+        self.top_outpath = []
+        self.top_content = []
+
+        self.top_info           = [] 
+        self.top_info_filelist  = [] 
+        self.top_sub_modules    = [] 
+        self.top_timescale      = []
+        self.top_incl_imp       = [] 
+        self.top_para           = [] 
+        self.top_localpara      = [] 
+        self.top_struct         = [] 
+        self.top_wiring         = [] 
+        self.top_code           = [] 
+
+    # Generation of para and ports in format .xxx(xxx)
     def gen_para_port(self, lst, default_connect):
         content = []
         try:
@@ -26,7 +44,7 @@ class ic_cnct:
             print("Input list in gen_para_port function is empty, propably no parameter")
         return content 
 
-
+    # Extract the parameters and ports
     def get_para_port(self, path2block, detail=False):
         dic_para = {"parameter": [], "default_value": []}
         dic_port = {"direction": [], "width": [], "portname": []}
@@ -68,10 +86,11 @@ class ic_cnct:
         else:
             return dic_para["parameter"], dic_port["portname"]
     
-    def cnct_blk(self, blockname, path2block, output2path="", default_connect=False):
+    # Connect the single block
+    def cnct_blk(self, blockname, instance_name, path2block, output2path="", default_connect=False, doprint=True):
         para, port = self.get_para_port(path2block, detail=False)
         content = []
-        content.append(f"{blockname} i_{blockname} #(")
+        content.append(f"{blockname} {instance_name} #(")
         content = content + self.gen_para_port(para, default_connect=True)
         content.append(f")(")
         content = content + self.gen_para_port(port, default_connect=default_connect)
@@ -80,7 +99,72 @@ class ic_cnct:
             with open(output2path, "w") as f:
                 for line in content:
                     f.write(f"{line}\n")
-        for i in content:
-            print(i)
+        if doprint:
+            for i in content:
+                print(i)
         return content
+
+    # Connect multiple blocks
+    def _cnct_blks_readjson(self, jsonfile, output2path):
+        f = open(jsonfile)
+        self.top_json = json.load(f)
+        f.close()
+        self.top_outpath = output2path
+        #if self.debug: print("all json info is:\n", self.top_json)
+
+    def _get_info(self):
+        self.top_info           = self.top_json["top_info"]
+        self.top_info_filelist  = self.top_json["top_info"]["filelist"]
+        self.top_sub_modules    = self.top_json["top_sub_modules"]
+        self.top_timescale      = self.top_json["top_timescale"]
+        self.top_incl_imp       = self.top_json["top_incl_imp"]
+        self.top_para           = self.top_json["top_para"]
+        self.top_localpara      = self.top_json["top_localpara"]
+        self.top_struct         = self.top_json["top_struct"]
+        self.top_wiring         = self.top_json["top_wiring"]
+        self.top_code           = self.top_json["top_code"]
+        #if self.debug: print("The top info is:\n", self.top_info)
+
+    def _cnct_blks_gen1(self):
+        content = self.top_content
+
+        # Add the includes and imports firstly
+        if self.top_incl_imp["include"] != []:
+            for item in self.top_incl_imp["include"]:
+                content.append(f"`include \"{item}\"")
+        if self.top_incl_imp["import"] != []:
+            for item in self.top_incl_imp["import"]:
+                content.append(f"import {item}::*;")
+        content.append(f"\n")
+
+        # create top module name
+        content.append(f"module {self.top_info['top_name']} #(")
+        content.append(f")(")
+        content.append(f");\n")
+
+        # gen sub modules
+        for i in range(len(self.top_sub_modules)):
+            module_name   = self.top_sub_modules[i]['module_name']
+            instance_name = self.top_sub_modules[i]['instance_name']
+            if self.top_sub_modules[i]["instance_name"] == "":
+                instance_name = f"i_{self.top_sub_modules[i]['module_name']}"
+
+            if self.top_info_filelist[module_name] == "":
+                sys.exit(f"module path is not given in filelist!!")
+            else:
+                path2block = self.top_info_filelist[module_name]
+            content.append(f"// Anchor {instance_name}")
+            content = content + self.cnct_blk(module_name, instance_name, path2block, default_connect=False, doprint=False)
+            content.append(f"\n")
+        self.top_content = content
+
+    def cnct_blks(self, jsonfile, output2path, debug=False):
+        self.debug = debug
+        self._cnct_blks_readjson(jsonfile, output2path)
+        self._get_info()
+        self._cnct_blks_gen1()
+        if output2path:
+            with open(output2path, "w") as f:
+                for line in self.top_content:
+                    f.write(f"{line}\n")
 
