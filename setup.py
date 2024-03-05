a303492926b1a51d2a3a67fc1b4a1b846e0d9d33
yuanchaov <yuanchaov@gmail.com>
2024-03-05 18:22:45 +0100

    Fix setup issue

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-import setuptools
+from setuptools import setup
 
 setup(
-   name="ictoolbox",
-   version="0.0.1",
-   description="IC toolbox",
-   author="Chaowei Yuan",
-   #author_email="",
-   packages=["ictoolbox"],
-   install_requires=["json", "jinja2", "numpy"],
+    name='ictoolbox',
+    version='0.0.1',
+    description='IC toolbox',
+    author='Chaowei Yuan',
+    #author_email='',
+    packages=['ic_cnct'],
+    install_requires=["Jinja2", "numpy", "pandas"],
 )
